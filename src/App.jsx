2b78dbc3ae842e1891c84fb5d51d7415f46bba0c
leonardo mendes <leonardo.smendescastilho@gmail.com>
2024-07-26 21:52:25 -0300

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
 import { Header } from './components/Header';
 import { Home } from './pages/Home/Home.jsx';
+import { Checkout } from './pages/Checkout/Checkout.jsx';
+import { Purchase } from './pages/Purchase/Purchase.jsx';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
 	return (
 		<>
 			<Header />
-			<Home />
+			<Routes>
+				<Route
+					path='/'
+					element={<Home />}
+				/>
+				<Route
+					path='/checkout'
+					element={<Checkout />}
+				/>
+				<Route
+					path='/history'
+					element={<Purchase />}
+				/>
+			</Routes>
 		</>
 	);
 }
