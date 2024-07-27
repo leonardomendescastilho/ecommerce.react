c78cb3e38768c44d0e51d266df0253110f6d065c
leonardo mendes <leonardo.smendescastilho@gmail.com>
2024-07-26 22:15:17 -0300

    add dinamic header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,39 @@
 import logo from '.././assets/logo/hashtag.svg';
 import { UserButtons } from './UserButtons';
+import { useLocation } from 'react-router-dom';
 
 export function Header() {
-	return (
-		<>
-			<header
-				className='flex sticky top-0 shadow-xl shadow-slate-400 bg-slate-950
-			 text-slate-200 px-8 py-4 justify-between items-end text-xl z-10'
-			>
-				<a href='/'>
-					<img
-						className='h-16 px-2'
-						src={logo}
-						alt='Logo da Hashtag Programação'
-					/>
-				</a>
-				<UserButtons />
-			</header>
-		</>
+	const { pathname } = useLocation();
+
+	const completeHeader = (
+		<header
+			className='flex sticky top-0 shadow-xl shadow-slate-400 bg-slate-950
+ text-slate-200 px-8 py-4 justify-between items-end text-xl z-10'
+		>
+			<a href='/'>
+				<img
+					className='h-16 px-2'
+					src={logo}
+					alt='Logo da Hashtag Programação'
+				/>
+			</a>
+			<UserButtons />
+		</header>
 	);
+
+	const simpleHeader = (
+		<header
+			className='flex sticky top-0 shadow-xl shadow-slate-400 bg-slate-950
+ text-slate-200 px-8 py-4 justify-between items-end text-xl z-10'
+		>
+			<a href='/'>
+				<img
+					className='h-12 px-2'
+					src={logo}
+					alt='Logo da Hashtag Programação'
+				/>
+			</a>
+		</header>
+	);
+	return <>{pathname === '/' ? completeHeader : simpleHeader}</>;
 }
